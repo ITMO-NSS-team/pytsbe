c254550d6399abdb5ee06c5f3c5cd2ff42c7ca7a
Dreamlone <mik_sar@mail.ru>
2022-06-01 18:37:48 +0300

    add multivariate reports

--- examples/multivariate_prepare_report.py
+++ examples/multivariate_prepare_report.py
@@ -5,16 +5,16 @@ def prepare_tables_with_metric():
     """
     Demonstration of how to aggregate results and calculate metric for already
     calculated cases.
-    Task: univariate forecasting
+    Task: multivariate forecasting
     """
-    metrics_processor = MetricsReport(working_dir='./example_launch')
+    metrics_processor = MetricsReport(working_dir='./example_multivariate_launch')
     timeouts_table = metrics_processor.time_execution_table(aggregation=['Library', 'Dataset'])
     print('Information about execution times:')
     print(timeouts_table)
 
     # Calculate and display information about metrics during validation
     metrics_table = metrics_processor.metric_table(metrics=['MAE', 'SMAPE'],
-                                                   aggregation=['Library', 'Dataset'])
+                                                   aggregation=['Library', 'Dataset', 'Horizon'])
     print('\nInformation about metrics:')
     print(metrics_table)
 
---
+++ examples/multivariate_prepare_visualizations.py
@@ -0,0 +1,26 @@
+from pytsbe.report.visualisation import Visualizer
+
+
+def prepare_plots():
+    """
+    Demonstration of how to use Visualizer to display plots
+    Task: multivariate forecasting
+    """
+    plots_creator = Visualizer(working_dir='./example_multivariate_launch',
+                               folder_for_plots=None)
+    # The graphs show how long it takes to learn the models
+    plots_creator.execution_time_comparison()
+
+    # Visualize information about metrics for different cases
+    plots_creator.metrics_comparison(metrics=['MAE', 'SMAPE'])
+
+    # For each time series plot competitors libraries
+    plots_creator.compare_forecasts()
+
+    # Visualize one library but for different launches
+    plots_creator.compare_launches(library='FEDOT')
+    # TODO fix AttributeError: 'MultivariateTimeSeriesDatasets' object has no attribute 'get_time_series_by_label'
+
+
+if __name__ == '__main__':
+    prepare_plots()
