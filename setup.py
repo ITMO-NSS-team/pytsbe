fa33c11484c4fd33914f132d514dd42578053510
Dreamlone <mik_sar@mail.ru>
2022-06-21 18:47:52 +0300

    setup instructions

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from os.path import dirname, join
+
+
+def read(*names, **kwargs):
+    with open(join(dirname(__file__), *names), encoding=kwargs.get('encoding', 'utf8')) as fh:
+        return fh.read()
+
+
+def extract_requirements(file_name):
+    return [r for r in read(file_name).split('\n') if r and not r.startswith('#')]
+
+
+def get_requirements():
+    requirements = extract_requirements('requirements.txt')
+    return requirements
+
+
+setuptools.setup(
+    name="pytsbe",
+    version="0.1",
+    author="NSS Team",
+    description="Python time series forecasting benchmark tool",
+    keywords='time series, forecasting, benchmark',
+    long_description_content_type="text/markdown",
+    url="https://github.com/ITMO-NSS-team/pytsbe",
+    python_requires='>=3.7',
+    install_requires=get_requirements(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
